28ff27a6d26bfb11ce51b9b1952733243eae6b9b
jeremyamram <52257264+jeremyamram@users.noreply.github.com>
2019-12-27 20:15:30 +0100

    cleanup the application

---
+++ src/component/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../assets/App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to Vezear
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
