f32fc23e3ee5af69af17853b218f48480b3f6b9a
Tish Chungoora <tishal82@gmail.com>
2019-12-29 13:46:07 +0000

    Implemented fetching of products and product categories sorted in alphabeticaly order; CategorySelector correctly populates the product categories

--- src/component/search/CategorySelector.jsx
+++ src/component/search/CategorySelector.jsx
@@ -2,15 +2,14 @@ import React, { Component } from "react";
 
 export default class CategorySelector extends Component {
   render() {
+    const { productCategories } = this.props;
+
     return (
       <div>
-        <h5 className="pb-2">Search by category:</h5>
+        <h5 className="pb-2">Filter by category:</h5>
         <select class="form-control" id="categorySelector">
           <option>All</option>
-          <option>Option 2</option>
-          <option>Option 3</option>
-          <option>Option 4</option>
-          <option>Option 5</option>
+          {productCategories.map(category => <option>{category.name}</option>)}
         </select>
       </div>
     );
--- src/container/Search.jsx
+++ src/container/Search.jsx
@@ -3,21 +3,51 @@ import ProductCollection from "./ProductCollection";
 import SearchBar from "../component/search/SearchBar";
 import CategorySelector from "../component/search/CategorySelector";
 
+const API_ENDPOINT = "http://localhost:3000";
+
 export default class Search extends Component {
+  state = {
+    productCategories: [],
+    products: []
+  };
+
+  getProductCategories = () => {
+    fetch(`${API_ENDPOINT}/product_categories`)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({
+          productCategories: data.sort((a, b) => a.name.localeCompare(b.name))
+        })
+      );
+  };
+
+  getProducts = () => {
+    fetch(`${API_ENDPOINT}/products`)
+      .then(response => response.json())
+      .then(data => this.setState({ products: data.sort((a, b) => a.name.localeCompare(b.name)) }));
+  };
+
+  componentDidMount() {
+    this.getProductCategories();
+    this.getProducts();
+  }
+
   render() {
+    const { productCategories } = this.state;
+
     return (
       <div className="container">
-          <div className="row bg-light border m-3 p-3">
-            <div className="col p-3">
-                <SearchBar />
-            </div>
-            <div className="col p-3">
-                <CategorySelector />
-            </div>
+        <div className="row bg-light border m-3 p-3">
+          <div className="col p-3">
+            <SearchBar />
           </div>
-          <div className="row p-3">
-            <ProductCollection />
+          <div className="col p-3">
+            <CategorySelector productCategories={productCategories} />
           </div>
+        </div>
+        <div className="row p-3">
+          <ProductCollection />
+        </div>
       </div>
     );
   }
