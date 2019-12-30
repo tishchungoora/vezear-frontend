ccd41dd39d83120f3006e06346071ed73dbc0662
jeremyamram <52257264+jeremyamram@users.noreply.github.com>
2019-12-30 11:17:06 +0100

    questions functionality

--- src/component/Question.js
+++ src/component/Question.js
@@ -1,9 +1,28 @@
 import React from "react";
 
-export default function Question({ question }) {
+export default function Question({ question, options, currentQuestion }) {
   return (
-    <div>
-      <h3>{question}</h3>
+    <div className="mb-5">
+      <div className="d-flex">
+        <h3 className="mr-3">{currentQuestion}.</h3>
+        <div>
+          <h3>{question}</h3>
+          {options.map(option => (
+            <div class="form-check">
+              <input
+                type="radio"
+                class="form-check-input"
+                id={option}
+                value={option}
+                name="radio"
+              />
+              <label class="form-radio-label" for={option}>
+                {option}
+              </label>
+            </div>
+          ))}
+        </div>
+      </div>
     </div>
   );
 }
--- src/container/Questions.js
+++ src/container/Questions.js
@@ -1,25 +1,78 @@
 import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
 import Question from "../component/Question";
 
-export default class Questions extends Component {
+class Questions extends Component {
   state = {
     questions: [
-      { question: "lorem 1", options: [], answer: "" },
-      { question: "lorem 2", options: [], answer: "" },
-      { question: "lorem 3", options: [], answer: "" },
-      { question: "lorem 4", options: [], answer: "" }
-    ]
+      { question: "lorem 1", options: ["a", "b", "c", "d"] },
+      { question: "lorem 2", options: ["a", "b", "c", "d"] },
+      { question: "lorem 3", options: ["a", "b", "c", "d"] },
+      { question: "lorem 4", options: ["a", "b", "c", "d"] }
+    ],
+    currentQuestion: 1
   };
+
+  handleNextQuestionChange = () => {
+    const { questions, currentQuestion } = this.state;
+    if (currentQuestion < questions.length) {
+      this.setState({
+        currentQuestion: currentQuestion + 1
+      });
+    }
+  };
+
+  handlePreviousQuestionChange = () => {
+    const { currentQuestion } = this.state;
+    if (currentQuestion !== 1) {
+      this.setState({
+        currentQuestion: currentQuestion - 1
+      });
+    }
+  };
+
+  handleSubmit = () => {
+    this.props.history.push("/");
+  };
+
   render() {
+    const { questions, currentQuestion } = this.state;
+
     return (
-      <div>
-        <h2 className="text-center">Questions</h2>
-        <div>
-          {this.state.questions.map(res => (
-            <Question question={res.question} />
-          ))}
+      <div className="container mt-5">
+        <h2 className="mb-4">Questions</h2>
+        <div className="mb-4">
+          <button
+            className="btn btn-warning mr-5"
+            onClick={this.handlePreviousQuestionChange}
+          >
+            Previous Question
+          </button>
+          <button
+            className={`btn ${
+              currentQuestion === questions.length
+                ? "btn-success"
+                : "btn-primary"
+            }`}
+            onClick={
+              currentQuestion === questions.length
+                ? this.handleSubmit
+                : this.handleNextQuestionChange
+            }
+          >
+            {currentQuestion === questions.length ? "Submit" : "Next Question"}
+          </button>
+        </div>
+        <div className="">
+          <Question
+            question={questions[currentQuestion - 1].question}
+            options={questions[currentQuestion - 1].options}
+            currentQuestion={currentQuestion}
+          />
         </div>
       </div>
     );
   }
 }
+
+export default withRouter(Questions);
