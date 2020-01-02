83f0b332a16f7a04089a59526104bcd5dc09dea6
Tish Chungoora <tishal82@gmail.com>
2020-01-02 20:29:08 +0000

    Added a banner for the landing page and placeholders for user instructions

--- src/component/App.js
+++ src/component/App.js
@@ -2,10 +2,18 @@ import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>Welcome to Vezearrrrrr</p>
-      </header>
+    <div className="App container-fluid">
+      <div id="banner" className="row justify-content-center pb-3">
+      <img className="img-fluid" src="banner.png" alt="Vezear" />
+      </div>
+      <div className="row justify-content-center">
+          <h1 className="display-4">How it works...</h1>
+        </div>
+      <div className="row justify-content-center">
+          <div className="col-4"></div>
+          <div className="col-4"></div>
+          <div className="col-4"></div>
+      </div>
     </div>
   );
 }
